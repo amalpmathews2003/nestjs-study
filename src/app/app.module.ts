aa7808df3f079f3c06108922c97bc805e8e6ba40
Amal P Mathews <amalpmathews2003@gmail.com>
2022-07-01 12:08:38 +0530

    added some controllers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
-
+import { StduentController } from 'src/student/student.controller';
+import { StudentTeacherController } from 'src/teacher/student.controller';
+import { TeacherController } from 'src/teacher/teacher.controller';
 @Module({
   imports: [],
+  controllers: [StduentController, TeacherController, StudentTeacherController],
 })
 export class AppModule {}
